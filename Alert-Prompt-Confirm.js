f807f3f5bdc2fad0b3565ca6a09f8cd685471de3
Ahsan Shakeel <ahsanshakeel1994@gmail.com>
2020-02-18 15:12:39 +0500

    Alert Prompt Confirm

--- Alert-Prompt-Confirm.js
+++ Alert-Prompt-Confirm.js
@@ -40,10 +40,3 @@
 
 // Confirm Syntax:
 // confirm("Press OK if you like my website");
-
-
-
-// Declare two variables: admin and name.
-// Assign the value "John" to name.
-// Copy the value from name to admin.
-// Show the value of admin using alert (must output “John”).
