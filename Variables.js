a4a13345654d0269dae2586cd0a6d7fe6da72c3c
Ahsan Shakeel <ahsanshakeel1994@gmail.com>
2020-02-18 15:07:17 +0500

    Variables

---
+++ Variables.js
@@ -0,0 +1,23 @@
+//What is Variable?
+
+// Varaibles in simple means just a name or some logical words you use to store something so 
+// you can recall it or reuse it.
+
+// Variable Syntax:
+// var name = "Ehsan";
+
+// To create a variable in Vanilla JavaScript we use "var" keyword.
+
+// var skills;
+// skills = "JavaScript"
+
+// in the above example 
+
+// var is a keyword through which we make variables.
+
+// var skills means we just declare a variable with the name of "skills" 
+// but we are not assigning any value into it. It means at that line value of 
+// variable "skills" is "undefined".
+
+// and in the next line we just assign a value with the keyword "=" and the value
+// is "JavaScript"
