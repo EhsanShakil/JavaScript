f8420975c48dae008c7d9156778d0e8b3b2b78d1
Ahsan Shakeel <ahsanshakeel1994@gmail.com>
2020-02-18 16:34:53 +0500

    Data Types

---
+++ Data-Types.js
@@ -0,0 +1,15 @@
+// There are two Data Type in JavaScript Primitive Data Types and Composite (Non-primitive) Data Type.
+
+// Primitive Data Types:
+
+// 1) String.
+// 2) Number.
+// 3) Symbol.
+// 4) Boolean.
+// 5) Undefined.
+// 6) Null.
+// 7) Symbol.
+
+// Composite (Non-primitive) Data Types:
+
+// 1) Object. (In JavaScript Objects, Arrays and Functions all have Data type Object)
