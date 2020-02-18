374f9d50944edf40e330a6522ba87011fb278994
Ahsan Shakeel <ahsanshakeel1994@gmail.com>
2020-02-18 15:09:59 +0500

    Alert Prompt Confirm

---
+++ Alert-Prompt-Confirm.js
@@ -0,0 +1,49 @@
+// What is Alert, Prompt and Confirm?
+
+
+// Alert:
+
+// It's just a pops up window on a screen and stop you to work or scroll down until you press "OK"
+// there is no "Cancel" button in this alert function.
+
+// Alert Syntax:
+// alert('Welcome to my website');
+
+
+
+
+// Prompt:
+
+// Prompt is just like a alert its show you pops up but with 2 differences first it takes some 
+// tittle or question and second you can also put some default value in it but it's not necessary 
+// to provide default values. User can put their answer if they want and then press "OK" other wise 
+// simply press cancel if they don't want to submit anything or press "Cancel" in this case the value will 'Null' which means
+// empty.
+
+// Prompt Syntax:
+// with default value
+// prompt("What's your name?", "e.g: Ehsan");
+
+// The first one is a question and second one is a default answer
+
+// without default value
+// prompt("What's your name?");
+
+
+
+
+// Confirm:
+
+// This function is also similar to other ones it also shows pops up to the user but it does not
+// take any answer in this confirm function it shows to button "OK" or "Cancel" if user press "OK" 
+// it shows true and if user press "Cancel" it shows false.
+
+// Confirm Syntax:
+// confirm("Press OK if you like my website");
+
+
+
+// Declare two variables: admin and name.
+// Assign the value "John" to name.
+// Copy the value from name to admin.
+// Show the value of admin using alert (must output “John”).
